7ee84db5135f96e5d9fd24620974be41dcc91041
NekoTheDev <mtmn261509@gmail.com>
2025-08-26 20:45:41 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://<NekoTheDev>.github.io/NTD-Portfolio/",
+  site: "https://NekoTheDev.github.io/NTD-Portfolio/",
   base: "/NTD-Portfolio/",
   outDir: "dist",
 });
-
-
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
